0c232e6a1eb3a0f78568352f55bbabd4636bb30a
ovidionavarro <ovidionavarro90@gmail.com>
2025-01-10 05:07:09 -0500

    form control

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
+// import { useRef } from 'react'
 import './App.css'
+import { useState } from 'react'
 import { Movies } from './components/Movies'
 import { UseMovie } from './Hooks/UseMovie'
 
 
 function App() {
   const {mappedMovies}=UseMovie()
-    
+  const [query,setQuery]=useState('')
+
+//forma no controlada
+  // const handleSubmit=(e)=>{
+  //   //se utiliza el name del input, sin state ni ref
+  //   e.preventDefault()
+  //   const {query}=Object.fromEntries(new window.FormData(e.target))
+  // console.log(query)       
+  // }
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log({query})
+  }
+
+  const handleChange=(e)=>{
+    setQuery(e.target.value)
+  }
+
+
+
+
   return(
     <div className='page'>
         <h1> SearchMovies</h1>
         <header>
-          <form className='form'>
-            <input placeholder='avengers, startwars' />
+          <form className='form' onSubmit={handleSubmit}>
+            <input value={query} onChange={handleChange} name='query' placeholder='avengers, startwars' />            
             <button type='submit'>Search</button>
           </form>
         </header>
