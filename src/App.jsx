87fed244003327320a8aea8746097b11ae4d8c7e
ovidionavarro <ovidionavarro90@gmail.com>
2025-01-09 23:53:57 -0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import { useRef } from 'react'
 import './App.css'
-import { Movies } from './components/Movies'
-import { UseMovies } from './Hooks/UseMovies'
 
 function App() {
-  const inputRef=useRef()
-  const {movies:mapMovies}=UseMovies()
-  
-  const handleSubmit=(event)=>{//recuperar el input del dom
-    event.preventDefault()
-    const inputValue=inputRef.current.value
-    console.log(inputValue)
-  }
   
 
   return(
-      <div className='page' >
-        <header>
+      <div>
         <h1>Search Movies</h1>
-        <div>
-          <form className='form' onSubmit={handleSubmit}>
-            <input 
-              ref={inputRef}
-              placeholder='avengers,star wars'  
-            />
-            <button type='submit'>Search</button>
-          </form>
-          </div>
-      </header>
-      <main>
-        <Movies movies={mapMovies}/>
-      </main>
+        
     </div>
   )
  
